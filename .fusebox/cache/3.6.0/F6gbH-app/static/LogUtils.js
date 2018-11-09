3944418778f3f824ef4567cfeaeace4ddee7e2f9
richard koehl <koehl.richard@gmail.com>
2018-11-09 22:51:20 +1030

    Remove folder .fusebox

--- .fusebox/cache/3.6.0/F6gbH-app/static/LogUtils.js
+++
@@ -1,9 +0,0 @@
-module.exports = { contents: "function logDeepest(message, args = \"\") {\n    if (__debugLevel >= __DEBUG_LEVEL_DEEPEST) {\n        console.log(message, args);\n    }\n}\nfunction logMinimum(message, args = \"\") {\n    if (__debugLevel >= __DEBUG_LEVEL_MIN) {\n        console.log(message, args);\n    }\n}\nmodule.exports = {\n    logDeepest, logMinimum\n};\n",
-dependencies: [],
-sourceMap: {},
-headerContent: undefined,
-mtime: 1541764102074,
-devLibsRequired : undefined,
-ac : undefined,
-_ : {}
-}
